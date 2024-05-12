10c6b5a287b51adbf0491e27982106b76f9e01f8
Aloereed <huzheng.zg@qq.com>
2024-05-12 17:09:24 +0000

    range

--- procedures/algo_cpp/range_triangle_core.cpp
+++ procedures/algo_cpp/range_triangle_core.cpp
@@ -18,15 +18,43 @@
 using namespace lgraph_api;
 using namespace lgraph_api::olap;
 
-size_t TriangleCountCommon(ParallelVector<size_t>& num_triangle,
+size_t binarySearch(size_t _key, const AdjUnit<Empty> *_list, size_t _left, size_t _right)
+    { // 一定返回的是最小的当前值或第一个大于k的值
+        int _mid;
+        while (_left < _right)
+        {
+            _mid = (_right + _left) / 2;
+            if ((_list+_mid)->neighbour < _key)
+                _left = _mid + 1;
+            else
+                _right = _mid;
+        }
+        return _left;
+    }
+
+size_t RangeTriangleCountCommon(ParallelVector<size_t>& num_triangle,
     AdjList<Empty> &list_a, AdjList<Empty> &list_b, size_t a, size_t b) {
     size_t local_count = 0;
     AdjUnit<Empty> *ptr_a = list_a.begin();
     AdjUnit<Empty> *ptr_b = list_b.begin();
+    size_t b1 = 0, f1 = list_a.end()-list_a.begin(), b2 = 0, f2 = list_b.end()-list_b.begin();
+    if(f1==0||f2==0||(ptr_a+b1)->neighbour>(ptr_b+f2-1)->neighbour || (ptr_b+b2)->neighbour>(ptr_a+f1-1)->neighbour)
+        return 0;
+    if ((ptr_a+b1)->neighbour < (ptr_b+b2)->neighbour)
+        b1 = binarySearch((ptr_b+b2)->neighbour, ptr_a, b1, f1);
+    else if ((ptr_a+b1)->neighbour > (ptr_b+b2)->neighbour)
+        b2 = binarySearch((ptr_a+b1)->neighbour, ptr_b, b2, f2);
+    if ((ptr_a+f1-1)->neighbour > (ptr_b+f2-1)->neighbour)
+        f1 = binarySearch((ptr_b+f2-1)->neighbour+1, ptr_a, b1, f1);
+    else if ((ptr_a+f1-1)->neighbour < (ptr_b+f2-1)->neighbour)
+        f2 = binarySearch((ptr_a+f1-1)->neighbour+1, ptr_b, b2, f2);
+
     size_t pre_a = -1;
     size_t pre_b = -1;
     size_t threshold = (a < b) ? a : b;
-    while (ptr_a != list_a.end() && ptr_b != list_b.end()
+    ptr_a += b1;
+    ptr_b += b2;
+    while (ptr_a != list_a.begin()+f1 && ptr_b != list_b.begin()+f2
         && ptr_a->neighbour < threshold && ptr_b->neighbour < threshold) {
         if (pre_a == ptr_a->neighbour) {
             ptr_a++;
@@ -56,20 +84,20 @@ size_t TriangleCountCommon(ParallelVector<size_t>& num_triangle,
     return local_count;
 }
 
-int triangle_compare(const void *a, const void *b) {
+int range_triangle_compare(const void *a, const void *b) {
     const AdjUnit<Empty> *ptr_a = (const AdjUnit<Empty> *)a;
     const AdjUnit<Empty> *ptr_b = (const AdjUnit<Empty> *)b;
     return ptr_a->neighbour > ptr_b->neighbour ? 1 : -1;
 }
 
-size_t TriangleCore(OlapBase<Empty>& graph, ParallelVector<size_t>& num_triangle) {
+size_t RangeTriangleCore(OlapBase<Empty>& graph, ParallelVector<size_t>& num_triangle) {
     auto active = graph.AllocVertexSubset();
     active.Fill();
     graph.ProcessVertexActive<size_t>(
             [&](size_t vtx) {
                 auto edges = graph.OutEdges(vtx);
                 qsort(edges.begin(), graph.OutDegree(vtx),
-                        sizeof(AdjUnit<Empty>), triangle_compare);
+                        sizeof(AdjUnit<Empty>), range_triangle_compare);
                 return 1;
             }, // 升序排列邻居
             active);
@@ -88,7 +116,7 @@ size_t TriangleCore(OlapBase<Empty>& graph, ParallelVector<size_t>& num_triangle
                     }
                     if (src < dst) {
                         AdjList<Empty> neighbour_adj = graph.OutEdges(dst);
-                        local_count += TriangleCountCommon(num_triangle,
+                        local_count += RangeTriangleCountCommon(num_triangle,
                                         src_adj, neighbour_adj, src, dst);
                     }
                 }
